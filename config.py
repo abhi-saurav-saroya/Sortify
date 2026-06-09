c468db7d2fe47ccd7e3dcad8b436fc707aad38c9
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-06-09 21:48:31 +0530

    Add file extension mappings

---
+++ config.py
@@ -0,0 +1,51 @@
+DEFAULT_CATEGORY = "Others"
+
+FILE_TYPES = {
+    # Images
+    ".jpg": "Images",
+    ".jpeg": "Images",
+    ".png": "Images",
+    ".gif": "Images",
+    ".webp": "Images",
+
+    # Documents
+    ".pdf": "Documents",
+    ".doc": "Documents",
+    ".docx": "Documents",
+    ".txt": "Documents",
+    ".ppt": "Documents",
+    ".pptx": "Documents",
+    ".xls": "Documents",
+    ".xlsx": "Documents",
+
+    # Audio
+    ".mp3": "Audio",
+    ".wav": "Audio",
+
+    # Videos
+    ".mp4": "Videos",
+    ".mkv": "Videos",
+    ".avi": "Videos",
+
+    # Archives
+    ".zip": "Archives",
+    ".rar": "Archives",
+    ".7z": "Archives",
+
+    # Source Code
+    ".c": "Source Code",
+    ".cpp": "Source Code",
+    ".h": "Source Code",
+    ".hpp": "Source Code",
+    ".py": "Source Code",
+    ".java": "Source Code",
+    ".js": "Source Code",
+
+    # Executables
+    ".exe": "Executables",
+    ".msi": "Executables",
+
+    # Object Files
+    ".o": "Build Files",
+    ".obj": "Build Files"
+}
