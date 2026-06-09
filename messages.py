f421fab42f4258cd1961feae906972f4c5821c8b
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-06-09 22:49:19 +0530

    user facing texts

---
+++ messages.py
@@ -0,0 +1,23 @@
+WELCOME_MESSAGE = """
+          ================================================
+                            SORTIFY
+                      Turning chaos into order.
+                        File Organizer Tool
+          ================================================
+        """
+    
+ABOUT_SORTIFY = """
+                                Sortify
+            A Python-based file organizer that automatically
+             sorts files into categorized folders based on
+                           their extensions.
+                            Created by Abhi.
+            """
+    
+MAIN_MENU = """
+MAIN MENU:
+    1. Organize Folder
+    2. View Supported Extensions
+    3. About
+    4. Exit
+"""
